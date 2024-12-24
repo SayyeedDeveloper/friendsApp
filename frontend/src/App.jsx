1b0d590bd57de6b421878822c41243ef09cf9306
Tursunov Muhammadsayyid <114228094+SayyeedDeveloper@users.noreply.github.com>
2024-12-24 20:26:07 +0500

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+
+    return (
+        <div className={'bg-sky-800 h-100vh text-white h-[100vh]'}>
+            <Navbar/>
+            <div className={'container my-1 md:my-4 '}>
+                <h1 className={'text-3xl font-bold'}>Hello World</h1>
+            </div>
+            <h3>Salom</h3>
+        </div>
+    )
+}
+
+export default App
