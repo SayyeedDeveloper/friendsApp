9ac2ba7e2a659ff2fe7ec282728399847130c394
Tursunov Muhammadsayyid <114228094+SayyeedDeveloper@users.noreply.github.com>
2024-12-27 17:21:24 +0500

    dark light mode

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,31 @@
-import Navbar from './components/Navbar'
+import { useState, useEffect } from 'react';
+import Navbar from './components/Navbar';
 
 function App() {
+    const [mode, setMode] = useState(() => localStorage.getItem('mode') || 'light');
+
+
+    const toggleMode = () => {
+        const newMode = mode === 'light' ? 'dark' : 'light';
+        setMode(newMode);
+        localStorage.setItem('mode', newMode);
+    };
+
+    useEffect(() => {
+        if (mode === 'dark') {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+        }
+    }, [mode]);
 
     return (
-        <div className={'bg-sky-700 h-100vh text-white h-[100vh]'}>
-            <Navbar/>
-            <div className={'container my-1 md:my-4 '}>
-                <h1 className={'text-3xl font-bold'}>Hello World</h1>
-            </div>
-            <h3>Salom</h3>
+        <div className="min-h-screen text-center bg-white dark:bg-sky-800 text-sky-800 dark:text-white">
+            <Navbar mode={mode} toggleMode={toggleMode}/>
+            <h1 className="text-xl  p-3 md:p-5 md:text-2xl lg:text-3xl font-bold uppercase">My Besties 🔥</h1>
+
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,34 +1,31 @@
-const Navbar = () => {
-    const colorMode = 'dark';
-    return (
-        <div className={'bg-sky-600 w-full p-4 px-7 border- flex justify-between'}>
-            <h1 className={'text-white text-xl font-bold'}>Friends</h1>
-            <div className={'flex gap-3'}>
-                <button>
-                    {
-                        colorMode === 'light' ?
-                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor"
-                             className="bi bi-moon-fill" viewBox="0 0 16 16">
-                            <path
-                                d="M6 .278a.77.77 0 0 1 .08.858 7.2 7.2 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277q.792-.001 1.533-.16a.79.79 0 0 1 .81.316.73.73 0 0 1-.031.893A8.35 8.35 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.75.75 0 0 1 6 .278"/>
-                        </svg> : <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor"
-                                      className="bi bi-brightness-high-fill" viewBox="0 0 16 16">
-                            <path
-                                d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708"/>
-                        </svg>
-                    }
+import { AiFillMoon } from "react-icons/ai";
+import { IoSunny } from "react-icons/io5";
+import { FaCirclePlus } from "react-icons/fa6";
+import PropTypes from 'prop-types';
 
+const Navbar = ({ mode, toggleMode }) => {
+    return (
+        <div className="w-full p-4 px-10 lg:px-20 border-b dark:border-gray-400 flex justify-between">
+            <h1 className="text-xl md:text-2xl lg:text-3xl font-bold text-sky-800 dark:text-white">Friends</h1>
+            <div className="flex gap-3">
+                <button onClick={toggleMode} className={'text-xl md:text-2xl lg:text-3xl text-sky-800 dark:text-white'}>
+                    {mode === 'light' ? (
+                        <IoSunny/>
+                    ) : (
+                        <AiFillMoon />
+                    )}
                 </button>
                 <button>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor"
-                         className="bi bi-plus-circle" viewBox="0 0 16 16">
-                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
-                        <path
-                            d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
-                    </svg>
+                    <FaCirclePlus className="text-2xl lg:text-3xl text-sky-800 dark:text-white"/>
                 </button>
             </div>
-</div>
-)
-}
-export default Navbar
+        </div>
+    );
+};
+
+Navbar.propTypes = {
+    mode: PropTypes.string.isRequired,
+    toggleMode: PropTypes.func.isRequired,
+};
+
+export default Navbar;
